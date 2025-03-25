6d6ef0a2c222f4fb64b4be0169900c2ec5cb42c2
Oladejo Jephthah <jephthahooh@gmail.com>
2025-03-25 07:10:31 +0100

    general routes fixed

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 
-
 Route::prefix('v1')->group(function () {
     require __DIR__ . '/api/auth.php';
     // require __DIR__ . '/api/user.php';
