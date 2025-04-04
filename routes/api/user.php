89ab7b7fc8766d4940a3b81f2bb952a16e9fcb23
Oladejo Jephthah <jephthahooh@gmail.com>
2025-04-04 09:12:11 +0100

    user added

--- routes/api/user.php
+++ routes/api/user.php
@@ -12,6 +12,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
     
 });
