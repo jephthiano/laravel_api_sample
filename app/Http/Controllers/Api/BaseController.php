8671a840cbd216009782bf4395a8225efd7c468f
Oladejo Jephthah <jephthahooh@gmail.com>
2025-04-07 08:58:11 +0100

    latest

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -26,7 +26,6 @@ class BaseController extends Controller
 
 
 
-
         
         if ($e instanceof QueryException) {
             $errorData = (env('APP_ENV') === 'local' || env('APP_ENV') === 'development') ? ['error' => $e->getMessage()] : [];
