45e399d7315aa13a75dded5cc06d94414cebff8f
Oladejo Jephthah <jephthahooh@gmail.com>
2025-04-15 08:56:00 +0100

    latest

--- routes/api/user.php
+++ routes/api/user.php
@@ -17,7 +17,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 
-
     
     // /api/users?page=2 and so on.....
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
