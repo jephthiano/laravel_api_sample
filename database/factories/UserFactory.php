41ad551700b05bf9806fa8c31b67d5db29ffb09a
Oladejo Jephthah <jephthahooh@gmail.com>
2025-04-22 09:14:09 +0100

    seeder updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -43,7 +43,6 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
-
         
     }
 
